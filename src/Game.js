49675d97cbabb343e21a0f74a3481035a2ace3b8
camcfarlin <camcfarlin@gmail.com>
2018-11-03 10:40:49 -0600

    contructor
    contructor for cards

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,21 @@
 import React, { Component } from 'react';
+import './Game.css';
+import CardView from './CardView';
+import MemoryCards from './MemoryCards';
+
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.onCardClicked = this.onCardClicked.bind(this);
+    this.onPlayAgain = this.onPlayAgain.bind(this);
+    this.memoryCards = new MemoryCards();
+  }
+
+  componentWillMount() {
+    this.initGame();
+  }
+
 
 
 export default Game;
